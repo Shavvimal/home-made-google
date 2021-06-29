765f18728665fffa86c3843bb65bca68f55d058e
Shav <shavhugan@gmail.com>
2021-06-29 15:29:48 +0100

    files added

--- server/app.js
+++ server/app.js
@@ -1,2 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+const port = 3000;
+
+
+
+
+
+
+
+app.use(cors());
+app.listen(port, () => console.log(`Express departing now from http://localhost:${port}`))
+
